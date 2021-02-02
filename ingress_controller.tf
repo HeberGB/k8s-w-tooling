125434444b34d6816b1367758445cde76a347798
hebergb <hebergb98@gmail.com>
2021-02-02 10:38:59 -0600

    feat(k8s): replace aic with nginx

--- ingress_controller.tf
+++ ingress_controller.tf
@@ -1,44 +1,10 @@
-##############################
-# REQUIREMENTS TO APPLY THIS #
-##############################
-#
-# 1. Create IAM OIDC provider
-# eksctl utils associate-iam-oidc-provider \
-#     --region <region-code> \
-#     --cluster <your-cluster-name> \
-#     --approve
-
-# 2. Create an IAM policy called AWSLoadBalancerControllerIAMPolicy
-# aws iam create-policy \
-#     --policy-name AWSLoadBalancerControllerIAMPolicy \
-#     --policy-document file://ingress-iam-policy.json
-# Take note of the policy ARN that is returned
-
-# 3. Create a IAM role and ServiceAccount for the AWS Load Balancer controller, use the ARN from the step above
-# eksctl create iamserviceaccount \
-# --cluster=<cluster-name> \
-# --namespace=kube-system \
-# --name=aws-load-balancer-controller \
-# --attach-policy-arn=arn:aws:iam::<AWS_ACCOUNT_ID>:policy/AWSLoadBalancerControllerIAMPolicy \
-# --approve
-
 resource "helm_release" "ingress_controller" {
-  name = "ingress-controller"
+  name      = "ingress-controller"
   namespace = "kube-system"
 
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  version    = "3.22.0"
+  chart      = "ingress-nginx"
 
-  set {
-    name = "clusterName"
-    value = local.cluster_name
-  }
-  set {
-    name = "serviceAccount.create"
-    value = "false"
-  }
-  set {
-    name = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+  values = [local.helm_chart_ingress_controller_values]
 }
